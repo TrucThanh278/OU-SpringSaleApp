224bf91b3918c52eaed2a3abe4956455e60f60f1
Truc Nguyen <trucnguyenthanh56@gmail.com>
2024-07-06 15:53:30 +0700

    Finish first excercise

--- HibernateDemo/src/main/java/com/mycompany/hibernatedemo/HibernateDemo.java
+++ HibernateDemo/src/main/java/com/mycompany/hibernatedemo/HibernateDemo.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.hibernatedemo;
 
 import com.ntt.repository.impl.CategoryRepositoryImpl;
 import com.ntt.repository.impl.ProductRepositoryImpl;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -16,10 +17,17 @@ public class HibernateDemo {
 
     public static void main(String[] args) {
 //        CategoryRepositoryImpl s = new CategoryRepositoryImpl();
-//        s.getCates().forEach(c -> System.out.println(c.getName()));\
+//        s.getCates().forEach(c -> System.out.println(c.getName()));\        
+
+//        ProductRepositoryImpl s = new ProductRepositoryImpl();
+//        s.getProducts(null).forEach(p -> System.out.printf("%s - %s \n", p.getName(), p.getPrice()));
+
+        Map<String, String> params = new HashMap<>();
+//        params.put("q", "iPhone");
+//        params.put("fromPrice", "28000000");
+        params.put("cateId", "2");
 
         ProductRepositoryImpl s = new ProductRepositoryImpl();
-        s.getProducts(null).forEach(p -> System.out.printf("%s - %s \n", p.getName(), p.getPrice()));
-        
+        s.getProducts(params).forEach(p -> System.out.printf("%s - %s \n", p.getName(), p.getPrice()));
     }
 }
--- HibernateDemo/src/main/java/com/ntt/repository/impl/ProductRepositoryImpl.java
+++ HibernateDemo/src/main/java/com/ntt/repository/impl/ProductRepositoryImpl.java
@@ -6,10 +6,12 @@ package com.ntt.repository.impl;
 
 import com.mycompany.hibernatedemo.HibernateUtils;
 import com.ntt.pojo.Product;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -26,6 +28,37 @@ public class ProductRepositoryImpl {
             
             Root root = q.from(Product.class);
             q.select(root);
+            
+            
+            List<Predicate> predicates = new ArrayList<>();
+            
+            //Search by product name
+            String kw = params.get("q");
+            if (kw != null && !kw.isEmpty()){
+                Predicate p1 = b.like(root.get("name"), String.format("%%%s%%", kw));
+                q.where(p1);
+            }
+            
+            
+            String fromPrice = params.get("fromPrice");
+            if (fromPrice != null && !fromPrice.isEmpty()){
+                Predicate p2 = b.greaterThanOrEqualTo(root.get("price"), Double.parseDouble(fromPrice));
+                predicates.add(p2);
+            }
+            
+            String toPrice = params.get("toPrice");
+            if (toPrice != null && !toPrice.isEmpty()){
+                Predicate p3 = b.greaterThanOrEqualTo(root.get("price"), Double.parseDouble(fromPrice));
+                predicates.add(p3);
+            }
+            
+            String cateId = params.get("cateId");
+            if (cateId != null & !cateId.isEmpty()){
+                Predicate p4 = b.equal(root.get("category"), Integer.parseInt(cateId));
+                predicates.add(p4);
+            }
+            
+            q.where(predicates.toArray(Predicate[]::new));
             
             Query query = s.createQuery(q);
             return query.getResultList();
